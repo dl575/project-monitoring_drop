0737ae2165fb78718602df60473a4bde18344d68
Daniel Lo <dl575@cornell.edu>
2012-11-28 13:50:13 -0500

    UMC exits when no more monitoring.

--- gem5-stable/tests/monitoring/umc.c
+++ gem5-stable/tests/monitoring/umc.c
@@ -16,11 +16,17 @@ int main(int argc, char *argv[]) {
 
   bool metadata[1024];
 
-  for(i = 0; i < 50; i++) {
+  while(1) {
     // Read FIFO data
     memcpy(&data, (void *)(0x30000000), 3*sizeof(int) + sizeof(bool));
 
-    printf("%x : m[%x] = %x ", data.instAddr, data.memAddr, data.data);
+    // If no more monitoring packets, exit
+    if (data.instAddr == 0) {
+      printf("Finished monitoring\n");
+      return 0;
+    }
+
+    printf("%x : m[%08x] = %x ", data.instAddr, data.memAddr, data.data);
     // Store
     if (data.store) {
       printf("store\n");
@@ -31,9 +37,11 @@ int main(int argc, char *argv[]) {
       printf("load\n");
       if (metadata[(data.memAddr >> 2) % 1024] == 0) {
         printf("UMC error\n");
+        // Exit if UMC error
+        return 1;
       }
     }
   }
 
-  return 0;
+  return 1;
 }
