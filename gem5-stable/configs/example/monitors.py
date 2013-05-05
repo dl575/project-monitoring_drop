47778cb70a20b5d54de3c99dec347ff5718319bb
Daniel Lo <dl575@cornell.edu>
2013-05-05 02:21:08 -0400

    Software dropping DIFT.

--- gem5-stable/configs/example/monitors.py
+++ gem5-stable/configs/example/monitors.py
@@ -1,6 +1,10 @@
 monitor = os.environ["MONITOR"]
-# Set up monitoring filter
+
+#######################################
+# UMC
+#######################################
 if monitor == "UMC":
+  # Set up monitoring filter
   MainCPUClass.monitoring_filter_load = True
   MainCPUClass.monitoring_filter_store = True
   # Load the invalidation file
@@ -11,23 +15,44 @@ if monitor == "UMC":
   monitor_bin = "umc_drop"
   #monitor_bin = "umc"
 elif monitor == "UMC_FULL":
+  # Set up monitoring filter
   MainCPUClass.monitoring_filter_load = True
   MainCPUClass.monitoring_filter_store = True
   # Define monitoring executable
   monitor_bin = "umc"
+
+#######################################
+# LRC
+#######################################
 elif monitor == "LRC":
+  # Set up monitoring filter
   MainCPUClass.monitoring_filter_call = True
   MainCPUClass.monitoring_filter_ret = True
   # Define monitoring executable
   monitor_bin = "lrc_drop"
   #monitor_bin = "lrc"
+
+#######################################
+# DIFT
+#######################################
+# Full monitoring always
 elif monitor == "DIFT_FULL":
+  # Set up monitoring filter
   MainCPUClass.monitoring_filter_load = True
   MainCPUClass.monitoring_filter_store = True
   MainCPUClass.monitoring_filter_intalu = True
   MainCPUClass.monitoring_filter_indctrl = True
   # Define monitoring executable
   monitor_bin = "dift"
+# Drop handled in software
+elif monitor == "DIFT_SWDROP":
+  # Set up monitoring filter
+  MainCPUClass.monitoring_filter_load = True
+  MainCPUClass.monitoring_filter_store = True
+  MainCPUClass.monitoring_filter_intalu = True
+  MainCPUClass.monitoring_filter_indctrl = True
+  # Define monitoring executable
+  monitor_bin = "dift_swdrop"
 else:
   raise Exception("Monitor not recognized: %s" % monitor)
 
