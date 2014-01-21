b5f1208030f3eddb1d0b691f966b8588dc909fbc
Daniel Lo <dl575@cornell.edu>
2014-01-20 22:09:23 -0500

    Updated scripts for power modeling on harddrop.

--- run/power/run_all_wcet.py
+++ run/power/run_all_wcet.py
@@ -3,6 +3,7 @@
 import glob
 import sys
 import os
+from parse_lib import *
 
 base_dir = sys.argv[1]
 
@@ -12,29 +13,44 @@ out_file = open(out_filename, 'w')
 out_file.close()
 
 # Find all directories with stats.txt files and using 0.1 overhead
+"""
 roots = []
 for root,dirs,files in os.walk(base_dir):
   for f in files:
     if f == "stats.txt":
       roots.append(root)
+"""
+include = ["100", "HWFILTER"]
+stf = get_txt_files(base_dir, include)
 
-# For each directory
-for root in roots:
-  print root
+# For each file
+for f in stf:
+  # Write out filename
+  print f
   out_file = open(out_filename, 'a')
-  out_file.write("%s\n" % root)
+  out_file.write("%s\n" % f)
   out_file.close()
-  # Copy in files
-  os.system("cp %s/* ." % root)
-  # Run McPat until successful
+
+  # Copy stats file to this directory
+  #os.system("cp -v %s ." % f)
+
+  # Run McPAT until successful
   ret = 1
   while ret != 0:
-    if "umc" in root:
-      raise Exception()
-      os.system("./m5-mcpat.pl stats.txt config.ini mim_umc_wcet.xml > output.xml")
+    # Run McPAT
+    if "UMC" in f or "LRC" in f:
+      os.system("rm -f results")
+      os.system("./m5-mcpat.pl %s config.ini mim_umc_wcet.xml > output.xml" % f)
     else:
-      os.system("./m5-mcpat.pl stats.txt config.ini mim_wcet.xml > output.xml")
+      os.system("rm -f results")
+      os.system("./m5-mcpat.pl %s config.ini mim_wcet.xml > output.xml" % f)
     ret = os.system("mcpat -infile output.xml -print_level 5 > results")
+
+    # When successful,
     if ret == 0:
-      os.system("cp results %s/mcpat.txt" % root)
+      # Save detailed results
+      #os.system("cp results %s/mcpat.txt" % root)
+      # write results to single outfile
       os.system("./parse_results.py results %s" % out_filename)
+
+sys.exit()
