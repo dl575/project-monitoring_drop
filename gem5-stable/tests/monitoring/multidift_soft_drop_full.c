254cd6ea01b4ad812be1f1766a5a15b01ea9a946
Tao Chen <tc466@cornell.edu>
2014-05-28 21:49:51 -0400

    Various fixes to core-based monitors.

--- gem5-stable/tests/monitoring/multidift_soft_drop_full.c
+++ gem5-stable/tests/monitoring/multidift_soft_drop_full.c
@@ -78,7 +78,7 @@ void* allocatePage(unsigned addr)
 inline DIFTTag readTag(unsigned physAddr)
 {
   char *page;
-  unsigned tagAddr = physAddr;
+  unsigned tagAddr = physAddr & 0xfffffffc;
   if (allocated(tagAddr)) {
     page = pagetable[getPageIndex(tagAddr)];
   } else {
@@ -91,7 +91,7 @@ inline DIFTTag readTag(unsigned physAddr)
 inline void writeTag(unsigned physAddr, DIFTTag tag)
 {
   char *page;
-  unsigned tagAddr = physAddr;
+  unsigned tagAddr = physAddr & 0xfffffffc;
   if (allocated(tagAddr)) {
     page = pagetable[getPageIndex(tagAddr)];
   } else {
