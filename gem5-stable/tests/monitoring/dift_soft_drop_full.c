a71574a3994cf44950d316bec2072495a3f26e91
Daniel Lo <dl575@cornell.edu>
2014-08-22 11:10:51 -0400

    Optimized full version of single-bit DIFT.

--- gem5-stable/tests/monitoring/dift_soft_drop_full.c
+++ gem5-stable/tests/monitoring/dift_soft_drop_full.c
@@ -23,9 +23,15 @@
 #define ISA_ARM
 
 #ifdef ISA_ARM
-  #define NUM_REGS 32
+  // Registers range from 1 to 36
+  #define NUM_REGS 37
+  // Exclude register 33 which is the constant zero register
+  #define ZERO_REG 33
+  //#define isISAReg(x) (x < NUM_REGS && x != ZERO_REG)
+  #define isISAReg(x) (x != ZERO_REG)
 #else
   #define NUM_REGS 32
+  #define isISAReg(x) (x < NUM_REGS)
 #endif
 
 #define MONITOR "[DIFT] "
@@ -33,11 +39,13 @@
 char tagmem[METADATA_ADDRESSES];
 bool tagrf[NUM_REGS];
 
+
 int main(int argc, char *argv[]) {
   register unsigned int temp;
   register unsigned int rd;
   register unsigned int rs;
   volatile register int error;
+  register bool tresult;
 
   // Set up monitoring
   INIT_MONITOR;
@@ -48,41 +56,45 @@ int main(int argc, char *argv[]) {
     // Grab new packet from FIFO. Block until packet available.
     POP_FIFO;
 
-    // Store
-    if (READ_FIFO_STORE) {
-      if (!(READ_FIFO_SETTAG)) {
-        // Get source register
-        rs = READ_FIFO_RS1;
-        // Propagate to destination memory addresses
-        temp = (READ_FIFO_MEMADDR >> 2);
-        if (tagrf[rs] == 1) {
-          // Bit mask to set taint
-          tagmem[temp >> 3] = tagmem[temp >> 3] | (1 << (temp&0x07));
-        } else {
-          // Bit mask to clear tag
-          tagmem[temp >> 3] = tagmem[temp >> 3] & ~(1 << (temp&0x07));
-        }
-      } else {
-        // settag operation
-        register int memend = (READ_FIFO_MEMEND >> 2);
-        for (temp = (READ_FIFO_MEMADDR >> 2); temp <= memend; ++temp) {
-          // Bit mask to set taint
-          tagmem[temp >> 3] = tagmem[temp >> 3] | (1 << (temp&0x07));
-        }
-      }
+    // ALU
+    if (READ_FIFO_INTALU) {
+      // on ALU instructions, propagate tag between registers
+      // Read source tags and determine taint of destination
+      rs = READ_FIFO_RS1;
+      tresult = tagrf[rs];
+      rs = READ_FIFO_RS2;
+      tresult |= tagrf[rs];
+      // Destination register
+      rd = READ_FIFO_RD;
+      // Set destination taint
+      tagrf[rd] = tresult;
+      tagrf[ZERO_REG] = false; // zero reg should always have 0 taint
     // Load
     } else if (READ_FIFO_LOAD) {
       // on load, propagate tag from memory to RF
       // Get destination register
       rd = READ_FIFO_RD;
       // Propagate from memory addresses
-      register bool tresult = false;
       temp = READ_FIFO_MEMADDR >> 2;
       // Pull out correct bit in memory to store int tag register file
       if ((tagmem[temp >> 3]) & (1 << (temp&0x7))) {
-        tresult = true;
+        tagrf[rd] = true;
+      } else {
+        tagrf[rd] = false;
       }
-      tagrf[rd] = tresult;
+    // Store
+    } else if (READ_FIFO_STORE) {
+      // Get source register
+      rs = READ_FIFO_RS1;
+      // Propagate to destination memory addresses
+      temp = (READ_FIFO_MEMADDR >> 2);
+      if (tagrf[rs] == 1) {
+        // Bit mask to set taint
+        tagmem[temp >> 3] = tagmem[temp >> 3] | (1 << (temp&0x07));
+      } else {
+        // Bit mask to clear tag
+        tagmem[temp >> 3] = tagmem[temp >> 3] & ~(1 << (temp&0x07));
+      }
     // Indirect control
     } else if (READ_FIFO_INDCTRL) {
       rs = READ_FIFO_RS1;
@@ -92,29 +104,11 @@ int main(int argc, char *argv[]) {
         // return -1;
         error = 1;
       }
-    // integer ALU
-    } else if (READ_FIFO_INTALU) {
-      // on ALU instructions, propagate tag between registers
-      // Read source tags and determine taint of destination
-      register bool tresult = false;
-      rs = READ_FIFO_RS1;
-      if (rs < NUM_REGS){
-        tresult |= tagrf[rs];
-      }
-      rs = READ_FIFO_RS2;
-      if (rs < NUM_REGS){
-        tresult |= tagrf[rs];
-      }
-      // Destination register
-      rd = READ_FIFO_RD;
-      // Set destination taint
-      if (rd < NUM_REGS) {
-        tagrf[rd] = tresult;
-      }
-    } else if ((READ_FIFO_SETTAG) && (READ_FIFO_SYSCALLNBYTES > 0)) {
+    } else if (READ_FIFO_SETTAG) {
       // syscall read instruction
-      for (temp = (READ_FIFO_SYSCALLBUFPTR >> 2); temp < (READ_FIFO_SYSCALLBUFPTR + READ_FIFO_SYSCALLNBYTES) >> 2; ++temp) {
-        // We use masks to store at bit locations based on last three bits
+      rs = READ_FIFO_SYSCALLBUFPTR >> 2;
+      rd = (READ_FIFO_SYSCALLNBYTES + rs) >> 2;
+      for (temp = rs; temp < rd; temp++) {
         tagmem[temp >> 3] = tagmem[temp >> 3] | (1 << (temp & 0x7));
       }
     } // inst type
